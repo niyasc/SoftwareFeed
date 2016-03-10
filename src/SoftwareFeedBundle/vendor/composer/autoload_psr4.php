7083db7d5a765b3082c15b108fb0d4782b6acaf4
Niyas C <niyasmonc@gmail.com>
2016-03-11 01:14:13 +0530

    Removed unused dependency

--- src/SoftwareFeedBundle/vendor/composer/autoload_psr4.php
+++ src/SoftwareFeedBundle/vendor/composer/autoload_psr4.php
@@ -6,5 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Symfony\\Component\\DependencyInjection\\' => array($vendorDir . '/symfony/dependency-injection'),
 );
