39175179a71942ce8cfc26cd15d67d1444acabc0
niyasc <niyasmonc@gmail.com>
2016-03-13 22:20:42 +0530

    DB Script updated

--- db.sql
+++ db.sql
@@ -60,7 +60,7 @@ CREATE TABLE `software` (
   UNIQUE KEY `UNIQ_77D068CF989D9B62` (`slug`),
   KEY `IDX_77D068CFC54C8C93` (`type_id`),
   CONSTRAINT `FK_77D068CFC54C8C93` FOREIGN KEY (`type_id`) REFERENCES `software_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `software` (
 
 LOCK TABLES `software` WRITE;
 /*!40000 ALTER TABLE `software` DISABLE KEYS */;
-INSERT INTO `software` VALUES (1,4,'Fedora','fedora','Fedora, formerly Fedora Core is an independently evolved GNU/Linux distribution sponsored by Red Hat, an open source giant. Fedora make use of RPM for package management. Fedora is often referred as a testing platform for Red Hat Enterprise Linux.','0a5f537bfb22ff52e6454de5867dfef2.svg');
+INSERT INTO `software` VALUES (1,4,'Fedora','fedora','Fedora, (formerly Fedora Core) is an independently evolved GNU/Linux distribution sponsored by Red Hat, an open source giant. Fedora make use of RPM for package management. Fedora is often referred as a testing platform for Red Hat Enterprise Linux.','7261a426f82f5d02ccccbaee0d5318db.svg'),(2,2,'Debian','debian','Debian is one of the oldest and biggest GNU/Linux project. It was started in 1993, by Ian Murdock. There are several distributions based on Debian. t comes with over 43000 packages, precompiled software bundled up in a nice format for easy installation on your machine.','d514bc99ec56845f4de28e3353dd317a.svg'),(4,2,'OpenSUSE','opensuse','openSUSE, formerly SUSE Linux and SuSE Linux Professional, is a Linux-based project and distribution sponsored by SUSE Linux GmbH and other companies. It is widely used throughout the world, particularly in Germany. The focus of its development is creating usable open-source tools for software developers and system administrators, while providing a user-friendly desktop, and feature-rich server environment.','e0f919886c7d67e03306e5d257626a9f.svg');
 /*!40000 ALTER TABLE `software` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,7 +118,7 @@ CREATE TABLE `software_type` (
   UNIQUE KEY `UNIQ_A9DB5DD5989D9B62` (`slug`),
   KEY `IDX_A9DB5DD5727ACA70` (`parent_id`),
   CONSTRAINT `FK_A9DB5DD5727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `software_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,7 +127,7 @@ CREATE TABLE `software_type` (
 
 LOCK TABLES `software_type` WRITE;
 /*!40000 ALTER TABLE `software_type` DISABLE KEYS */;
-INSERT INTO `software_type` VALUES (1,NULL,'System Software','System Software is a computer software that is responsible for providing services for other application software packages. It act as an intermediate layer between user applications and computer hardware.','system-software'),(2,1,'Operating System','Operating System is a system software that act as an intermediate layer between computer hardware and application software packages. Operating System is responsible for performing resources management tasks such as process management, memory management, device management and file management.','operating-system'),(3,NULL,'Application Software','Application Software refers to computer software packages which performs some end user actions. Application software can be either general purpose or special purpose.','application-software'),(4,2,'Linux or GNU/Linux','The term Linux is often referred to the operating system GNU/Linux. GNU/Linux is a free and open source operating system. It includes GNU shell developed by Free Software Foundation and Linux kernel. There are hundreds of distributions based on GNU shell and Linux Kernel.','linux-or-gnu-linux'),(5,2,'Microsoft Windows Family','Windows is one of the most widely used proprietary operating system. Windows is a graphical user interface based operating system developed, sold and owned by Microsoft. Active Windows families include Windows NT, Windows Embedded and Windows Phone; these may encompass subfamilies, e.g. Windows Embedded Compact (Windows CE) or Windows Server.','microsoft-windows-family'),(6,2,'OS X','OS X (originally Mac OS X) is a series of Unix-based graphical interface operating systems (OS) developed and marketed by Apple Inc. It is designed to run on Macintosh computers, having been pre-installed on all Macs since 2002. OS X is the fourth most popular general purpose OS; within the market of desktop, laptop and home computers, and by web usage, OS X is the second most widely used desktop OS after Windows. - Wikipedia','os-x');
+INSERT INTO `software_type` VALUES (1,NULL,'System Software','System Software is a computer software that is responsible for providing services for other application software packages. It act as an intermediate layer between user applications and computer hardware.','system-software'),(2,1,'Operating System','Operating System is a system software that act as an intermediate layer between computer hardware and application software packages. Operating System is responsible for performing resources management tasks such as process management, memory management, device management and file management.','operating-system'),(3,NULL,'Application Software','Application Software refers to computer software packages which performs some end user actions. Application software can be either general purpose or special purpose.','application-software'),(4,2,'Linux or GNU/Linux','The term Linux is often referred to the operating system GNU/Linux. GNU/Linux is a free and open source operating system. It includes GNU shell developed by Free Software Foundation and Linux kernel. There are hundreds of distributions based on GNU shell and Linux Kernel.','linux-or-gnu-linux'),(7,2,'BSD','BSD, Berkeley Software Distribution, was a Unix  like operating system developed and distributed by the Computer Systems Research Group (CSRG) of the University of California, Berkeley, from 1977 to 1995. Today the term \"BSD\" is often used non-specifically to refer to any of the BSD descendants which together form a branch of the family of Unix-like operating systems. Operating systems derived from the original BSD code remain actively developed and widely used.','bsd');
 /*!40000 ALTER TABLE `software_type` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -140,4 +140,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-13 16:26:22
+-- Dump completed on 2016-03-13 22:20:28
